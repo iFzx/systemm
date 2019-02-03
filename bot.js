cbeeac36e7b19fc1c0f6ef8c4d164e5be1446a60
iFzx <39328821+iFzx@users.noreply.github.com>
2019-02-03 22:59:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -300,7 +300,6 @@ client.on('message', message => {
               console.log(`Servers : [ " ${client.guilds.size} " ]`);
               console.log(`Users : [ " ${client.users.size} " ]`);
               console.log(`Channels : [ " ${client.channels.size} " ]`);
-              console.log('By : '+`${client.users.get("290908670529896448").username}`)
                client.user.setActivity("You .",{type: 'WATCHING'})
                client.user.setStatus("idle")
             });
